557598eb08bdd5e79625251937b7286fef9db16a
Kushtra <jax050505@gmail.com>
2019-11-06 12:30:39 +0100

    Heroku support added

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading weather...';
   messageTwo.textContent = '';
 
-  fetch('http://localhost:3000/weather?adress=' + encodeURI(location)).then((res) => {
+  fetch('/weather?adress=' + encodeURI(location)).then((res) => {
     res.json().then((data) => {
       if(data.error){
         messageOne.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const publicPath = path.join(__dirname, '../public');
