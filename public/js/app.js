4993abaf68859619d416345c6a0620fd5dfbddd7
Kushtra <jax050505@gmail.com>
2020-08-08 13:43:23 +0200

    Code cleanup v2

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,13 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message1');
 const messageTwo = document.querySelector('#message2');
-const day1 = document.querySelector('#day1');
-const day2 = document.querySelector('#day2');
-const day3 = document.querySelector('#day3');
-const day4 = document.querySelector('#day4');
-const day5 = document.querySelector('#day5');
-const day6 = document.querySelector('#day6');
+const days = [];
+days.push(document.querySelector('#day1'));
+days.push(document.querySelector('#day2'));
+days.push(document.querySelector('#day3'));
+days.push(document.querySelector('#day4'));
+days.push(document.querySelector('#day5'));
+days.push(document.querySelector('#day6'));
 
 const punch = "'";
 
@@ -16,12 +17,9 @@ weatherForm.addEventListener('submit', (e) => {
   const location = search.value;
   messageOne.textContent = 'Loading weather...';
   messageTwo.textContent = '';
-  day1.textContent = '';
-  day2.textContent = '';
-  day3.textContent = '';
-  day4.textContent = '';
-  day5.textContent = '';
-  day6.textContent = '';
+  days.forEach(day => {
+    day.textContent = '';
+  });
 
   fetch('/weather?adress=' + encodeURI(location)).then((res) => {
     res.json().then((data) => {
@@ -31,11 +29,9 @@ weatherForm.addEventListener('submit', (e) => {
         messageOne.textContent = ('Temperature in ' + data.location + ' is ' + data.temperature + '°C.');
         messageTwo.textContent = data.summary;
         day1.textContent = ('Tomorrow' + punch + 's highest temperature is ' + data.week[0].temperatureHigh + '°C and lowest is ' + data.week[0].temperatureLow + '°C. ' + data.week[0].summary);
-        day2.textContent = (data.weekName[0] + punch + 's highest temperature is ' + data.week[1].temperatureHigh + '°C and lowest is ' + data.week[1].temperatureLow + '°C. ' + data.week[1].summary);
-        day3.textContent = (data.weekName[1] + punch + 's highest temperature is ' + data.week[2].temperatureHigh + '°C and lowest is ' + data.week[2].temperatureLow + '°C. ' + data.week[2].summary);
-        day4.textContent = (data.weekName[2] + punch + 's highest temperature is ' + data.week[3].temperatureHigh + '°C and lowest is ' + data.week[3].temperatureLow + '°C. ' + data.week[3].summary);
-        day5.textContent = (data.weekName[3] + punch + 's highest temperature is ' + data.week[4].temperatureHigh + '°C and lowest is ' + data.week[4].temperatureLow + '°C. ' + data.week[4].summary);
-        day6.textContent = (data.weekName[4] + punch + 's highest temperature is ' + data.week[5].temperatureHigh + '°C and lowest is ' + data.week[5].temperatureLow + '°C. ' + data.week[5].summary);
+        for(let i=1;i<days.length;i++) {
+          days[i].textContent = (data.weekName[i-1] + punch + 's highest temperature is ' + data.week[i].temperatureHigh + '°C and lowest is ' + data.week[i].temperatureLow + '°C. ' + data.week[i].summary);
+        }
       }
     });
   });
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -8,30 +8,7 @@ const weather = (longitude, latitude, callback) => {
       callback('Unable to connecto to whether api, check your internet connection!', undefined);
     } else if(body.error) {
       callback('Unable to fetch whether for those coordinates, try again!', undefined);
-    }/* else {
-      callback(undefined, {
-        temperature: body.currently.temperature,
-        summary: body.hourly.summary,
-        day1sum: body.daily.data[0].summary,
-        day1max: Math.round(body.daily.data[0].temperatureHigh),
-        day1min: Math.round(body.daily.data[0].temperatureLow),
-        day2sum: body.daily.data[1].summary,
-        day2max: Math.round(body.daily.data[1].temperatureHigh),
-        day2min: Math.round(body.daily.data[1].temperatureLow),
-        day3sum: body.daily.data[2].summary,
-        day3max: Math.round(body.daily.data[2].temperatureHigh),
-        day3min: Math.round(body.daily.data[2].temperatureLow),
-        day4sum: body.daily.data[3].summary,
-        day4max: Math.round(body.daily.data[3].temperatureHigh),
-        day4min: Math.round(body.daily.data[3].temperatureLow),
-        day5sum: body.daily.data[4].summary,
-        day5max: Math.round(body.daily.data[4].temperatureHigh),
-        day5min: Math.round(body.daily.data[4].temperatureLow),
-        day6sum: body.daily.data[5].summary,
-        day6max: Math.round(body.daily.data[5].temperatureHigh),
-        day6min: Math.round(body.daily.data[5].temperatureLow)
-      });
-    }*/ else {
+    } else {
       callback(undefined, {
         temperature: body.currently.temperature,
         summary: body.hourly.summary,
